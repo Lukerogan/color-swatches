cce86aae7741a8118a55dfbaa66d3be0ebab0468
Luke Rogan <126Lrogan@frhsd.com>
2025-01-14 16:32:20 -0500

    Finished

--- js/js.js
+++ js/js.js
@@ -1,11 +1,34 @@
-// $(document).ready(function(){
-//     $(".blue").hover(function(){
-//       $(".main-img").css("background-color", "yellow");
-//       }, function(){
-//       $(this).css("background-color", "pink");
-//     });
-//   });
+$(".blue").hover(function(){
+    $(".main-img").attr("src", "img/bluecouch.jpg");
+  });
+$(".purple").hover(function(){
+    $(".main-img").attr("src", "img/purplecouch.jpg");
+  });
+$(".pink").hover(function(){
+    $(".main-img").attr("src", "img/pinkcouch.jpg");
+  });
+$(".red").hover(function(){
+    $(".main-img").attr("src","img/redcouch.jpg" );
+  });
+$(".orange").hover(function(){
+    $(".main-img").attr("src", "img/orangecouch.jpg");
+  });
+$(".yellow").hover(function(){
+    $(".main-img").attr("src", "img/yellowcouch.jpg");
+  });
+$(".green").hover(function(){
+    $(".main-img").attr("src", "img/greencouch.jpg");
+  });
+$(".white").hover(function(){
+    $(".main-img").attr("src", "img/whitecouch.jpg");
+  });
+$(".grey").hover(function(){
+    $(".main-img").attr("src", "img/greycouch.jpg");
+  });
+$(".brown").hover(function(){
+    $(".main-img").attr("src", "img/browncouch.jpg");
+  });
 
-document.querySelector('.blue').addEventListener('mouseover',function (e){
-    e.target.src = '../img/screenshot(6).png'
-})
+// document.querySelector('.blue').addEventListener('mouseover',function (e){
+//     e.target.src = '../img/screenshot(6).png'
+// })
